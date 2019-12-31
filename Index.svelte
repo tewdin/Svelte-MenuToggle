d9687072835d956c145f09bc34112c97e1b88335
Timo Anttila <info@tuspe.com>
2020-01-01 01:53:00 +0200

    Create Index.svelte

---
+++ Index.svelte
@@ -0,0 +1,9 @@
+<script>
+	let active = false;
+</script>
+
+// Add or remove class "show"
+<nav id="menu" class:show={active}></nav>
+
+// Toggle active true / false
+<button id="menu-toggler" class="icon" class:icon-menu={!active} class:icon-cancel={active} on:click={ () => active = !active}></button>
